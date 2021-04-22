f19a7a8c20b67ad1dff2262ff68939473e017cd8
Jack Center <jack.d.center@gmail.com>
2021-04-21 18:16:09 -0600

    added additional parameters and control.

--- config.py
+++ config.py
@@ -1,5 +1,30 @@
-def get_parameters():
-    alpha = 0.1
-    epsilon = 0.1
-    gamma = 0.9
-    iterations = 100
+def get_configuration():
+    config = {
+        "load_Q": False,
+        "train": True
+    }
+
+    return config
+
+
+def get_training_parameters():
+    params = {
+        "alpha": 0.1,
+        "epsilon": 0.1,
+        "gamma": 0.9,
+        "iterations": 400
+    }
+
+    return params
+
+
+def get_single_run_parameters():
+    params = {
+        "alpha": 0.1,
+        "epsilon": 0.0,
+        "gamma": 0.9,
+        "iterations": 1
+    }
+
+    return params
+
