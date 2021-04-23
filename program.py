b90ec881e03a6b2eec745d05bf670685a0f20706
Jack Center <jack.d.center@gmail.com>
2021-04-23 09:35:45 -0600

    improved layout, added matrix multiplication to sarsa lambda, and put default action in the configuration

--- program.py
+++ program.py
@@ -3,7 +3,6 @@ import numpy as np
 import config
 import simplePendulum
 
-# TODO: add default action = 0
 # TODO: add documentation
 
 
@@ -13,52 +12,114 @@ def main():
     params = config.get_training_parameters()
     env = simplePendulum.PendulumEnv()
 
-    env.reset()
+    params.update({"default_action_index": np.where(env.get_discrete_action_space() == 0)[0][0]})
 
+    # run_sarsa(env, cfg, params)
+    run_sarsa_lambda(env, cfg, params)
+
+    plot_policy(env, "sarsa_lambda", params)
+    run_policy(env, "sarsa_lambda", params)
+
+
+def run_sarsa(env, cfg, params):
     if cfg.get("load_Q"):
-        Q = np.load('Q_new.npy')
-
+        Q = np.load('data/Q_sarsa.npy')
+        episodes = np.load('data/E_sarsa.npy')
     else:
         Q = get_initial_Q(env)
-
-    print(Q)
-    plot_policy(env, Q, params.get("default_action_index"))
+        episodes = 0
 
     if cfg.get("train"):
-        for i in range(100):
-            if not i%100:
+        for i in range(params.get("episodes")):
+            if not i % 100:
                 print(i)
 
-            Q = sarsa(env, Q, params)
             env.reset()
+            Q = sarsa(env, Q, params)
+            episodes += 1
 
-        np.save('Q_new.npy', Q)
-
-    plot_policy(env, Q, params.get("default_action_index"))
-    run_policy(env, Q)
+        np.save('data/Q_sarsa.npy', Q)
+        np.save('data/E_sarsa.npy', episodes)
 
     env.close()
+    print(episodes)
+
+
+def run_sarsa_lambda(env, cfg, params):
+    if cfg.get("load_Q"):
+        Q = np.load('data/Q_sarsaLambda.npy')
+        N = np.load('data/N_sarsaLambda.npy')
+        episodes = np.load('data/E_sarsaLambda.npy')
+    else:
+        Q = get_initial_Q(env)
+        N = np.zeros(np.shape(Q))
+        episodes = 0
+
+    if cfg.get("train"):
+        for i in range(params.get("episodes")):
+            if not i % 100:
+                print(i)
+
+            env.reset()
+            Q, N = sarsa_lambda(env, Q, N, params)
+            episodes += 1
+
+        np.save('data/Q_sarsaLambda.npy', Q)
+        np.save('data/N_sarsaLambda.npy', N)
+        np.save('data/E_sarsaLambda.npy', episodes)
+
+    env.close()
+    print(episodes)
 
 
 def get_initial_Q(env):
 
-    x = env.state_space_discrete
-    u = env.action_space_discrete
+    x = env.get_discrete_state_space()
+    u = env.get_discrete_action_space()
 
     Q = np.zeros((len(x), len(u)))
 
     return Q
 
 
-def run_policy(env, Q):
+def plot_policy(env, algorithm, params):
+
+    if algorithm == "sarsa_lambda":
+        Q = np.load('data/Q_sarsaLambda.npy')
+
+    ud_idx = params.get("default_action_index")
+
+    x_list = env.state_space_discrete
+    x0_values = env.get_discrete_state_values(0)
+    x1_values = env.get_discrete_state_values(1)
+    u_values = env.get_discrete_action_values()
+    u_idx_list = [u.argmax() if sum(u) else ud_idx for u in Q]
+    u_list = [u_values[idx] for idx in u_idx_list]
+
+    actions = np.zeros((len(x0_values), len(x1_values)))
+    for (x0, x1), u in zip(x_list, u_list):
+        x = np.where(x0_values == x0)[0][0]
+        y = np.where(x1_values == x1)[0][0]
+        actions[x][y] = u
+
+    plt.imshow(actions, origin='lower')
+    plt.colorbar()
+    plt.show()
+
+
+def run_policy(env, algorithm, params):
+
+    if algorithm == "sarsa_lambda":
+        Q = np.load('data/Q_sarsaLambda.npy')
+
+    ud_idx = params.get("default_action_index")
 
     env.reset()
-
     for _ in range(400):
         env.render()
         x = observe(env)
         x_idx = env.get_state_index(x)
-        u_idx = policy(env, Q, x_idx, 0.0)
+        u_idx = policy(env, Q, x_idx, ud_idx)
         u1 = env.action_space_discrete[u_idx]
 
         step(env, u1)
@@ -70,7 +131,7 @@ def sarsa(env, Q, params):
     epsilon = params.get("epsilon")
     gamma = params.get("gamma")
     iterations = params.get("iterations")
-    ud_idx = params.get("default_action_idx")
+    ud_idx = params.get("default_action_index")
 
     x0 = observe(env)
     x0_idx = env.get_state_index(x0)
@@ -106,7 +167,7 @@ def sarsa_lambda(env, Q, N, params):
     gamma = params.get("gamma")
     iterations = params.get("iterations")
     lamb = params.get("lambda")
-    ud_idx = params.get("default_action_idx")
+    ud_idx = params.get("default_action_index")
 
     x0 = observe(env)
     x0_idx = env.get_state_index(x0)
@@ -116,22 +177,19 @@ def sarsa_lambda(env, Q, N, params):
 
     for _ in range(0, iterations):
 
-        N[(x0_idx[0], x0_idx[1], u0_idx)] += 1
+        N[(x0_idx, u0_idx)] += 1
         x1 = observe(env)
         x1_idx = env.get_state_index(x1)
 
-        u1_idx = policy(env, Q, x1, ud_idx, epsilon)
+        u1_idx = policy(env, Q, x1_idx, ud_idx, epsilon)
         u1 = env.action_space_discrete[u1_idx]
 
         step(env, u1)
 
-        delta = alpha * (r + gamma * Q[(x1_idx[0], x1_idx[1], u1_idx)] - Q[(x0_idx[0], x0_idx[1], u0_idx)])
+        delta = r + gamma * Q[(x1_idx, u1_idx)] - Q[(x0_idx, u0_idx)]
 
-        for x_idx in env.state_space_discrete:
-            for u_idx in env.action_space_discrete:
-
-                Q[(x_idx, u_idx)] += alpha*delta*N[(x_idx[0], u0_idx)]
-                N[(x_idx, u_idx)] *= gamma*lamb
+        Q += alpha*delta*N
+        N *= gamma*lamb
 
         x0 = x1
         x0_idx = x1_idx
@@ -141,7 +199,7 @@ def sarsa_lambda(env, Q, N, params):
 
         # TODO: delay until end of next step
 
-    return np.copy(Q)
+    return np.copy(Q), np.copy(N)
 
 
 def observe(env):
@@ -186,26 +244,6 @@ def print_environment_bounds(env):
     print(env.observation_space)
     print(env.observation_space.low)
     print(env.action_space.sample())
-
-
-def plot_policy(env, Q, ud_idx):
-
-    x_list = env.state_space_discrete
-    x0_values = env.get_discrete_state_values(0)
-    x1_values = env.get_discrete_state_values(1)
-    u_values = env.get_discrete_action_values()
-    u_idx_list = [u.argmax() if sum(u) else ud_idx for u in Q]
-    u_list = [u_values[idx] for idx in u_idx_list]
-
-    actions = np.zeros((len(x0_values), len(x1_values)))
-    for (x0, x1), u in zip(x_list, u_list):
-        x = np.where(x0_values == x0)[0][0]
-        y = np.where(x1_values == x1)[0][0]
-        actions[x][y] = u
-
-    plt.imshow(actions, origin='lower')
-    plt.colorbar()
-    plt.show()
 
 
 if __name__ == "__main__":
